9e06506f87656edbaadcb36fd65a06c2991bcfee
Naveen AutomationLabs <naveenanimation20@gmail.com>
2021-08-21 17:56:12 +0530

    first commit

---
+++ src/main/java/GItBasics/GItBasics/App.java
@@ -0,0 +1,13 @@
+package GItBasics.GItBasics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
